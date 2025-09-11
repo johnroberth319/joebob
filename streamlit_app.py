cfb9675e8bebddc325e72ea14b1731a470826f52
John Robert Hernandez <johnrhernandez319@gmail.com>
2025-09-11 12:28:20 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,19 +8,12 @@ def main():
     if menu == "Personal":
         page = st.sidebar.radio("Select a page", ["Home"])
         if page == "Home":
-            st.write("This is the Home Page.")
+            st.Page("Home.py", title="Home Page", icon="🏠")
 
     elif menu == "GCU":
         course = st.sidebar.selectbox("Choose a course", ["CST435"])
         if course == "CST435":
             page = st.sidebar.radio("Select a CST435 page", ["Overview", "Assignments", "Labs"])
-            
-            if page == "Overview":
-                st.write("CST435 Overview content")
-            elif page == "Assignments":
-                st.write("CST435 Assignments content")
-            elif page == "Labs":
-                st.write("CST435 Labs content")
 
 if __name__ == "__main__":
     main()
