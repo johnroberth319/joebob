cfd588d446e60125754c6480a6e43c44cff8333b
John Robert Hernandez <johnrhernandez319@gmail.com>
2025-09-11 11:47:43 -0700

    Page Configuration
    Test to place Filipino flag as page icon, alongside new title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="JoeBob - John's Streamlit Portfolio",
+    page_icon="🇵🇭",
+    layout="wide"
+)
+
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
