46e21ba100c1d9938c4d54d88c9857d51fc2ea07
John Robert Hernandez <johnrhernandez319@gmail.com>
2025-09-15 14:01:01 -0700

    Updated & Reorganized Home

--- Home.py
+++ Home.py
@@ -8,8 +8,11 @@ st.set_page_config(
 
 st.title("JoeBob - John's Streamlit Portfolio")
 st.header("Who am I?")
+st.write("My name is John Robert Hernandez.")
+st.write("Currently (Septmember 15th, 2025), I am a student at Grand Canyon University, going for a Bachelor's in Computer Science with a Big Data Emphasis.")
+st.header("What is JoeBob?")
 st.write("""
-    My name is John Robert Hernandez.
-    Currently, I am a student at Grand Canyon University, going for a Bachelor's in Computer Science with a Big Data Emphasis.
-    Graduating in Summer 2026, this portfolio is a collection of my work and projects done in my final semesters at GCU.
-""")
+    Graduating in Summer 2026, the JoeBob portfolio is a collection of my work and projects done in my final semesters at GCU.
+    With an aim to showcase my projects and skills in data science and machine learning for classes such as Deep Learning,
+    it includes various applications and experimentsI've conducted throughout my studies in the final year.
+""")    
