c2f02ac45b8677b54ab2ff357e68bbdef4780578
John Robert Hernandez <johnrhernandez319@gmail.com>
2025-09-15 14:07:39 -0700

    Added JoeBob Explanation

--- Home.py
+++ Home.py
@@ -6,13 +6,20 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title("JoeBob - John's Streamlit Portfolio")
-st.header("Who am I?")
+st.title("🐐 JoeBob - John's Streamlit Portfolio 🐐")
+st.header("Who am I? 🤓")
 st.write("My name is John Robert Hernandez.")
 st.write("Currently (Septmember 15th, 2025), I am a student at Grand Canyon University, going for a Bachelor's in Computer Science with a Big Data Emphasis.")
-st.header("What is JoeBob?")
+st.header("What is JoeBob? 🤔")
 st.write("""
     Graduating in Summer 2026, the JoeBob portfolio is a collection of my work and projects done in my final semesters at GCU.
     With an aim to showcase my projects and skills in data science and machine learning for classes such as Deep Learning,
     it includes various applications and experimentsI've conducted throughout my studies in the final year.
+""")
+st.subheader("Why JoeBob? 🧐")
+st.write("""
+    JoeBob is a nickname I came up with as an alias for myself based on my actual name.
+    With "Joe" being a reference to the first part of my first name "John" and 
+         "Bob" being a reference to the second part of my first name "Robert," I came up with JoeBob, 
+         a short and catchy name that is easy to remember.
 """)    
