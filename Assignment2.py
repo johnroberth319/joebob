8de4519832dc519b80be2ac7259c3779761565e3
John Robert Hernandez <johnrhernandez319@gmail.com>
2025-09-15 15:31:17 -0700

    Added Sections for Assignment 2

--- Assignment2.py
+++ Assignment2.py
@@ -7,8 +7,25 @@ st.set_page_config(
 )
 
 st.title("CST435 - Assignment 2")
-st.header("Artificial Neural Networks")
+st.header("Artificial Neural Networks - Optimal NBA team")
 st.subheader("by John Robert & Nolan")
+st.subheader("Project Description")
+st.write("""
+    TBD
+""")
+st.subheader("Problem Statement")
+st.write("""
+    TBD
+""")
+st.subheader("Algorithm Overview")
+st.write("""
+    TBD
+""")
+st.subheader("Analysis of Results")
+st.write("""
+    TBD
+""")
+st.subheader("References")
 st.write("""
     TBD
 """)
